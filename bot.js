277463e42d0d33a52b721968143bc9ac475c573e
Luis Espinola <lespinola@gmail.com>
2018-08-09 20:57:18 -0400

    asks questions and parses answers

--- bot.js
+++ bot.js
@@ -1,14 +1,46 @@
 const Symphony = require('symphony-api-client-node');
 
-Symphony.setDebugMode(true);
+const prompts = {
+	create: 'create zd ticket',
+	subject: 'Enter ticket subject:',
+	rvm: 'What RVM version are you using?',
+	runtime: 'What Runtime version are you using?',
+	details: 'Enter details of issue:'
+}
+
+let requests = {};
+let requestCtr = 0;
 
 const listenToMessages = (event, messages) => {
-	console.log(event);
 	messages.forEach((message, index) => {
+
 		const messageText = message.messageText.toLowerCase();
-		console.log(messageText);
+
+		if (messageText === prompts.create) {
+			requests[message.user.email] = {
+				previousPrompt: prompts.create
+			};
+			Symphony.sendMessage(message.stream.streamId, prompts.subject, null, Symphony.MESSAGEML_FORMAT);
+		} else if (requests[message.user.email] && requests[message.user.email].previousPrompt === prompts.create) {
+			requests[message.user.email].subject = message.messageText;
+			requests[message.user.email].previousPrompt = prompts.subject;
+			Symphony.sendMessage(message.stream.streamId, prompts.rvm, null, Symphony.MESSAGEML_FORMAT);
+		} else if (requests[message.user.email] && requests[message.user.email].previousPrompt === prompts.subject) {
+			requests[message.user.email].rvm = message.messageText;
+			requests[message.user.email].previousPrompt = prompts.rvm;
+			Symphony.sendMessage(message.stream.streamId, prompts.runtime, null, Symphony.MESSAGEML_FORMAT);
+		} else if (requests[message.user.email] && requests[message.user.email].previousPrompt === prompts.rvm) {
+			requests[message.user.email].runtime = message.messageText;
+			requests[message.user.email].previousPrompt = prompts.runtime;
+			Symphony.sendMessage(message.stream.streamId, prompts.details, null, Symphony.MESSAGEML_FORMAT);
+		} else if (requests[message.user.email] && requests[message.user.email].previousPrompt === prompts.runtime) {
+			requests[message.user.email].details = message.messageText;
+			requests[message.user.email].previousPrompt = prompts.details;
+		}
 	});
 }
+
+
 
 Symphony.initBot(__dirname + '/config.json')
 	.then((symAuth) => {
